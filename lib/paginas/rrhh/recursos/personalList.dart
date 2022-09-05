b35d9f437004b61065da23dfe2d762206764d670
Sojostar <juansojo@gmail.com>
2022-09-05 17:18:54 -0400

    erp_002

--- lib/paginas/rrhh/recursos/personalList.dart
+++ lib/paginas/rrhh/recursos/personalList.dart
@@ -1,16 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:erp/paginas/rrhh/model/PersonalModel.dart';
-import 'package:erp/paginas/rrhh/personal.dart';
 
-class PersonalList extends StatelessWidget {
-  final List<PersonalModel> _personas;
-
-  PersonalList(this._personas);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: _buildPersonalList(),
-    );
-  }
-}
