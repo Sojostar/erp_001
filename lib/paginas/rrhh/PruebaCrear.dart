0f72cfbbe9f5b4e71ca74c71c7999efd014f8895
Sojostar <juansojo@gmail.com>
2022-09-08 05:44:18 -0400

    erp_006

--- lib/paginas/rrhh/PruebaCrear.dart
+++ lib/paginas/rrhh/PruebaCrear.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mysql1/mysql1.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import 'package:file_picker/file_picker.dart';
 
 Future fn_query_pais(texto) async {
   // Open a connection (testdb should already exist)
@@ -19,6 +22,7 @@ Future fn_query_pais(texto) async {
 class PruebaCrear extends StatefulWidget {
   const PruebaCrear({super.key});
 
+  final String title = "Upload Image Demo";
   @override
   State<PruebaCrear> createState() => _PruebaCrearState();
 }
@@ -26,12 +30,65 @@ class PruebaCrear extends StatefulWidget {
 class _PruebaCrearState extends State<PruebaCrear> {
   final _formKey = GlobalKey<FormState>();
   final textoController = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
     textoController.dispose();
     super.dispose();
+  }
+
+/*
+final FilePickerResult? result = await FilePicker.platform.pickFiles( 
+  type: FileType.custom,
+  allowedExtensions: ['jpg', 'pdf', 'doc'],) ;
+*/
+
+  static final String uploadEndPoint =
+      'http://localhost/flutter_test/upload_image.php';
+  Future<XFile>? file;
+  String status = '';
+  String base64Image = '';
+  XFile? tmpFile;
+  String errMessage = 'Error Uploading Image';
+
+  void _setImageFileListFromFile(XFile? value) {
+    final file = value == null ? null : <XFile>[value];
+  }
+
+  chooseImage() {
+    setState(() {
+      final file = _picker.pickImage(source: ImageSource.gallery);
+    });
+    setStatus('');
+  }
+
+  setStatus(String message) {
+    setState(() {
+      status = message;
+    });
+  }
+
+  startUpload() {
+    setStatus('Uploading Image...');
+    if (null == tmpFile) {
+      setStatus(errMessage);
+      return;
+    }
+    String fileName = 'juan';
+    upload(fileName);
+  }
+
+  upload(String fileName) {
+    http.post(Uri.parse(uploadEndPoint), body: {
+      "image": base64Image,
+      "name": fileName,
+    }).then((result) {
+      setStatus(result.statusCode == 200 ? result.body : errMessage);
+    }).catchError((error) {
+      setStatus(error);
+    });
   }
 
   @override
@@ -88,7 +145,18 @@ class _PruebaCrearState extends State<PruebaCrear> {
                 child: Container(
                   padding: EdgeInsets.all(40.0),
                   child: Column(
-                    children: <Widget>[],
+                    children: <Widget>[
+                      TextButton(
+                        style: ButtonStyle(
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.blue),
+                        ),
+                        onPressed: () {
+                          chooseImage();
+                        },
+                        child: Text('TextButton'),
+                      )
+                    ],
                   ),
                 ),
               ),
